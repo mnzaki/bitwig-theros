893f1c6ceb136ed5474cd9c1d4e0a587a62d41b7
Mina Nagy Zaki <mnzaki@gmail.com>
2014-08-17 00:29:38 +0200

    Refined and improved.
    Channel based control separation.
    Different pages per channel.
    Empty channels freely assignable.

--- oxygen25.control.js
+++ oxygen25.control.js
@@ -1,13 +1,16 @@
 loadAPI(1);
 
-/**** Constants ****/
+/********************************* Constants *******************************/
 var MIDI_MAX_VAL = 128;
 //var ECHO_ID = "12";
 var SYSEX_HEADER = "F0 42 40 00 01 04 00";
 
 var noteInFilters = [];//"80????", "90????"];
 
-/**** Device discovery ****/
+var HIGHEST_CC = 119;
+var LOWEST_CC = 1;
+
+/****************************** Device discovery ***************************/
 host.defineController("M-Audio", "Oxygen 25", "1.0", "2B6AB540-B75A-11E3-A5E2-0800200C9A66");
 host.defineMidiPorts(1, 1);
 host.addDeviceNameBasedDiscoveryPair(["Oxygen 25"], ["Oxygen 25"]);
@@ -20,7 +23,7 @@ for (var i = 1; i < 9; i++) {
   host.addDeviceNameBasedDiscoveryPair(["Oxygen 25 MIDI " + i.toString()], ["Oxygen 25 MIDI " + i.toString()]);
 }
 
-/**** Mapping ****/
+/********************************* Mappings ********************************/
 // Views accessible to control callbacks
 var views = {
   application: null,
@@ -34,12 +37,13 @@ var views = {
   midiInPort: null,
   noteIn: null,
   controlPages: null,
+  userControls: null,
 };
 
 // MIDI CCs to labels
 var controls = {
   knobs: [71, 72, 73, 74, 75, 76, 77, 78],
-  sliders: [7],
+  slider: 7,
   c10: 31,
   loop: 113,
   stop: 116,
@@ -47,99 +51,149 @@ var controls = {
   rec: 118
 };
 
-// Page Definitions
+/******************************** Pages ************************************/
+// page definition:
+// { name: 'Some Page',
+//   mappings: [
+//    {on: an array of CC values, eg: controls.knobs or [44,45,46]
+//     control: function(chan, idx_in_cc_array) or bitwig control
+//              if it's a function, it's evalued once for each cc
+//              ONLY evaluted ONCE at startup
+//     controlAbs: same as control, but the value is set to the
+//                 absolute value in the CC message
+//     callback: function(idx_in_cc_array, chan, cc, new_val, last_val)
+//               callback to call when one there's a message with
+//               a matching CC
+//     always: true/false
+//                 if true, then mapping is active even when
+//                 ControlPages.setEnabled(false) was called
+//    },
+//    .....
+//  ]
+// }
+//
+// See the presets below for examples
 
-// common reusable mappings
-var globalMappings =
-[{
-  on: controls.c10,
-  always: true, // call even when controls are disabled!
-  callback: function (val) {
-    if (val > 0)
-      views.controlPages.selectNone();
-    else
-      views.controlPages.selectNext();
-  }
-}];
+// TODO give pages a callback to decide whether they are active
+//      depending on the current instrument, current view, etc
 
-// pages
-var pages =
+// Presets
+// don't edit these, but instead edit the variable 'pages' at the end of
+// the configuration area
+
+// each page handles a subset of device knobs
+var separateDevicePages =
 [{
-  name: 'device macros',
+  name: 'Device - Macros',
   mappings:
   [{
     on: controls.knobs,
-    mapControlRel: function (idx) {
+    control: function (chan, idx) {
       return views.primaryInstrument.getMacro(idx).getAmount();
     }
   }]
 },
 {
-  name: 'device parameters',
+  name: 'Device - Parameters',
   mappings:
   [{
     on: controls.knobs,
-    mapControlRel: function (idx) {
-      views.cursorDevice.setParameterPage(0);
+    control: function (chan, idx) {
+      //views.cursorDevice.setParameterPage(0);
       return views.cursorDevice.getParameter(idx);
     }
   }]
 },
 {
-  name: 'device common parameters',
+  name: 'Device - Common Parameters',
   mappings:
   [{
     on: controls.knobs,
-    mapControlRel: function (idx) {
+    control: function (chan, idx) {
       return views.cursorDevice.getCommonParameter(idx);
     }
   }]
 },
 {
-  name: 'device common parameters',
+  name: 'Device - Envelope Parameters',
   mappings:
   [{
     on: controls.knobs,
-    mapControlRel: function (idx) {
+    control: function (chan, idx) {
       return views.cursorDevice.getEnvelopeParameter(idx);
     }
   }]
 }];
 
-for (var p in pages) {
-  var mappings = pages[p].mappings;
-  if (!mappings) pages[p].mappings = globalMappings;
-  else pages[p].mappings =
-          pages[p].mappings.concat(globalMappings);
-}
+var mixerPages =
+[{
+  name: 'Mixer - Volumes',
+  mappings:
+  [{
+    on: controls.knobs,
+    control: function (chan, idx) {
+      return views.trackBank.getTrack(idx).getVolume();
+    }
+  }]
+},
+{
+  name: 'Mixer - Pan',
+  mappings:
+  [{
+    on: controls.knobs,
+    control: function (chan, idx) {
+      return views.trackBank.getTrack(idx).getPan();
+    }
+  }]
+}];
 
-/*mixerPage = new Page();
-mixerPage.onKnob = function(index, val)
-{
-  trackBank.getTrack(index).getPan().set(val, 128);
-}
-mixerPage.onSlider = function(index, val)
-{
-  trackBank.getTrack(index).getVolume().set(val, 128);
-}
-mixerPage.updateIndications = function()
-{
-  for ( var p = 0; p < 8; p++)
-  {
-    macro = primaryInstrument.getMacro(p).getAmount();
-    parameter = cursorDevice.getParameter(p);
-    track = trackBank.getTrack(p);
-    track.getVolume().setIndication(true);
-    track.getPan().setIndication(true);
-    parameter.setIndication(false);
-    macro.setIndication(false);
+// And finally, map each channel to a set of pages.
+// channels left empty will be free to be mapped live
+var channelPages = new Array(16);
+channelPages[0] = separateDevicePages;
+channelPages[1] = mixerPages
+
+// Global Mappings are active across channels
+// if mapped in a page, the page takes precedence
+var globalMappings =
+[{
+  on: controls.c10,
+  always: true, // call even when controls are disabled!
+  callback: function (idx, chan, cc, val, hist) {
+    if (val > 0)
+      views.controlPages.setEnabled(false);
+    else
+      views.controlPages.selectNext(chan);
   }
-}*/
-
-/*************************************************************/
-
-function init()
+},
 {
+  on: controls.play,
+  always: true,
+  callback: function (idx, chan, cc, val, hist) { views.transport.play() }
+},
+{
+  on: controls.stop,
+  always: true,
+  callback: function (idx, chan, cc, val, hist) { views.transport.stop() }
+},
+{
+  on: controls.loop,
+  always: true,
+  callback: function (idx, chan, cc, val, hist) { views.transport.toggleLoop() }
+},
+{
+  on: controls.rec,
+  always: true,
+  callback: function (idx, chan, cc, val, hist) {
+    views.cursorTrack.getArm().set(true);
+    views.transport.record();
+  }
+}];
+
+/***************************************************************************/
+/***************************************************************************/
+
+function init() {
   views.application = host.createApplication();
   views.transport = host.createTransport();
   views.trackBank = host.createTrackBank(8, 1, 0);
@@ -152,236 +206,222 @@ function init()
 
   views.noteIn = views.midiInPort.createNoteInput.apply(views.midiInPort, ["Oxygen 25 Keyboard"].concat(noteInFilters));
 
-  views.controlPages = new ControlPages(pages);
+  views.controlPages = new ControlPages(globalMappings, channelPages);
+  views.userControls = views.controlPages.userControls;
+
   views.midiInPort.setMidiCallback(
     function (status, data1, data2) {
       views.controlPages.onMidi(status, data1, data2)
   });
+
   //sendSysex(SYSEX_HEADER + "00 00 01 F7"); // Enter native mode
   // sendSysex(SYSEX_HEADER + "1F 10 00 F7"); //sysex dump request
 }
 
-function exit()
-{
+function exit() {
   //sendSysex(SYSEX_HEADER + "00 00 00 F7"); // Leave native mode
 }
 
-/*
-function onMidi(status, data1, data2)
-{
-  var cc = data1;
-  var val = data2;
-  //printMidi(status, cc, val);
-
-  if (status == 176)
-  {
-    if (withinRange(data1, CC.SLIDER1, CC.SLIDER5))
-    {
-      var index = data1 - CC.SLIDER1;
-      activePage.onSlider(index, val);
-      // trackBank.getTrack(index).getVolume().set(data2, 128);
-    }
-
-    else if (withinRange(data1, CC.SLIDER6, CC.SLIDER7))
-    {
-      var index = data1 - (CC.SLIDER1) - 1;
-      activePage.onSlider(index, val);
-    }
-    else if (cc == CC.SLIDER8)
-    {
-      var index = 7;
-      activePage.onSlider(index, val);
-    }
-    else if (cc == CC.SLIDER9)
-    {
-      masterTrack.getVolume().set(data2, 128);
-    }
-
-    else if (withinRange(data1, CC.KNOB1, CC.KNOB1 + 7))
-    {
-      var index = data1 - CC.KNOB1;
-      activePage.onKnob(index, val);
-    }
-    else if (cc == CC.KNOB9)
-    {
-      // var tempo = Math.round((val * 0.5)+10);
-      // transport.getTempo().set(val+30, 500);
-    }
-
-    if (val > 0) // deal with button presses here
-    {
-      if (withinRange(data1, CC.UPPER_BUTTON1, CC.UPPER_BUTTON8))
-      {
-        var index = data1 - CC.UPPER_BUTTON1;
-        trackBank.getTrack(index).getMute().toggle();
-      }
-      else if (withinRange(data1, CC.LOWER_BUTTON1, CC.LOWER_BUTTON8))
-      {
-        var index = data1 - CC.LOWER_BUTTON1;
-        trackBank.getTrack(index).getArm().toggle();
-      }
-      switch (data1)
-      {
-        case CC.TOGGLE_MODE_PAGE:
-          switchPage();
-          break;
-
-        case CC.TOGGLE_VIEW:
-          application.nextPerspective();
-          break;
-
-        case CC.PLAY:
-          isPlay ? transport.restart() : transport.play();
-          break;
-
-        case CC.STOP:
-          transport.stop();
-          break;
-
-        case CC.REC:
-          transport.record();
-          break;
-
-        case CC.REW:
-          transport.rewind();
-          break;
-
-        case CC.FF:
-          transport.fastForward();
-          break;
-
-        case CC.LOOP:
-          transport.toggleLoop();
-          break;
-      }
-    }
-  }
-}*/
-
-function onSysex(data)
-{
+function onSysex(data) {
   // printSysex(data);
 }
 
-var action_map = {
-  mapControl: function (action_cb, cc_idx, val) {
-    action_cb(cc_idx).set(val, MIDI_MAX_VAL);
-  },
-  mapControlRel: function (action_cb, cc_idx, val, hist) {
-    if (hist)
-      action_cb(cc_idx).inc(val-hist, MIDI_MAX_VAL);
-  },
-  callback: function (action_cb, cc_idx, val) {
-    action_cb(val);
-  },
-  triggerCallback: function (action_cb, cc_idx, val) {
-    if (val > 0) action_cb(val);
-  }
-};
+function Page(pageDesc, chan) {
+  this.name = pageDesc.name;
+  this.channel = chan;
+  this.ccMap = {};
+  this.ccCallback = {};
+  this.ccAlways = {}; // CCs that trigger even when controls are disabled
+  this.indicateCtrls = [];
 
-function ControlPages(page_descs)
-{
-  self = this;
-  self.enabled = true;
-  self.pages = [];
-  self.currentPageIdx = -1;
-  self.currentPage = null;
-  self.cc_val_history = {};
-
-  this.action_map = action_map;
-
-  for (var p in page_descs) {
-    self.registerPage(page_descs[p]);
-  }
-  this.selectPage(0);
-}
-
-function Page(action_map, page_desc) {
-  this.name = page_desc.name;
-  this.cc_map = {};
-  this.always_ccs = {}; // CCs that trigger even when controls are disabled
-  for (action in action_map)
-    this.cc_map[action] = {}
-  this.indicate_cbs = [];
-
-  for (var m in page_desc.mappings) {
-    m = page_desc.mappings[m];
+  for (var m in pageDesc.mappings) {
+    m = pageDesc.mappings[m];
     if (!(m.on instanceof Array)) {
       m.on = [m.on];
     }
     for (var o in m.on) {
-      for (var action in this.cc_map) {
-        if (m[action])
-          this.cc_map[action][m.on[o]] = [m[action], o];
-          if (m.always) {
-            for (var cc in m.on)
-              this.always_ccs[m.on[cc]] = true;
-          }
+      var cc = m.on[o];
+      var ctrl;
+      if (m.control) {
+        if (m.control.call)
+          ctrl = m.control(chan, o);
+        else
+          ctrl = m.control;
+        this.ccMap[cc] = [ctrl, false];
+      } else if (m.controlAbs) {
+        this.ccMap[cc] = [ctrl, true];
+      }
+      if (m.callback) {
+        this.ccCallback[cc] = [m.callback, o];
+      }
+      if (m.always) {
+        this.ccAlways[cc] = true;
+      }
+      if (m.indicate) {
+        if (m.indicate.call)
+          ctrl = m.indicate(chan, o);
+        else
+          ctrl = m.indicate;
+        this.indicateCtrls.push(ctrl);
       }
     }
-    if (m['indicate'])
-      this.indicate_cbs.push(m.on[o]);
   }
 }
 
-ControlPages.prototype.registerPage = function (page_desc) {
-  this.pages.push(new Page(this.action_map, page_desc));
-};
-ControlPages.prototype.selectNone = function () {
-  this.setEnabled(false);
+function ControlPages(globalMappings, channelPages) {
+  this.enabled = false;
+  var globalpageDesc = {name: 'Global', mappings: globalMappings};
+  this.globalPage = new Page(globalpageDesc, 0);
+  this.currentChannel = undefined;
+  this.channelPages = new Array(16);
+  this.currentPageIdx = new Array(16);
+  for (var i = 0; i < this.currentPageIdx.length; i++)
+    this.currentPageIdx[i] = 0;
+
+  this.currentPage = null;
+  this.ccValHistory = {};
+
+  this.userControls = host.createUserControls((HIGHEST_CC - LOWEST_CC + 1)*16);
+
+  // Label the controls
+  for (var cc = LOWEST_CC; cc <= HIGHEST_CC; cc++) {
+    for (var chan = 0; chan < 16; chan++)
+      this.getUserControl(chan, cc).setLabel("CC"+cc);
+  }
+
+  for (var ctrl_name in controls) {
+    if (controls[ctrl_name] instanceof Array) {
+      for (var i in controls[ctrl_name]) {
+          for (var chan = 0; chan < 16; chan++)
+            this.getUserControl(chan, controls[ctrl_name][i]).setLabel(ctrl_name+i);
+      }
+    }
+  }
+
+  for (var chan in channelPages)
+    for (var p in channelPages[chan])
+      this.registerPage(chan, channelPages[chan][p]);
+
+  this.selectPage(0, 0);
 }
+
+ControlPages.prototype.registerPage = function (chan, pageDesc) {
+  if (!this.channelPages[chan]) this.channelPages[chan] = [];
+  this.channelPages[chan].push(new Page(pageDesc, chan));
+};
 ControlPages.prototype.setEnabled = function (val) {
   this.enabled = val;
   this.indicateGUI(val);
-}
-ControlPages.prototype.selectPage = function (idx) {
+};
+ControlPages.prototype.selectPage = function (chan, idx) {
   if (this.enabled) this.setEnabled(false);
-  idx %= this.pages.length;
-  if (idx < 0) idx += this.pages.length;
-  this.currentPageIdx = idx;
-  this.currentPage = this.pages[idx];
+  var pages = this.channelPages[chan];
+  if (pages) {
+    idx %= pages.length;
+    if (idx < 0) idx += pages.length;
+    this.currentPageIdx[chan] = idx;
+    this.currentPage = pages[idx];
+  } else {
+    this.currentPage = null;
+  }
   this.setEnabled(true);
+
+  var msg = "Ch" + chan;
+  if (this.currentPage)
+    msg += " Page: " + this.currentPage.name;
+  host.showPopupNotification(msg);
 };
-ControlPages.prototype.selectNext = function () {
-  this.selectPage(this.currentPageIdx+1);
+ControlPages.prototype.selectNext = function (chan) {
+  if (!chan) chan = this.currentPage.channel;
+  if (chan != this.currentPage.channel)
+    this.selectPage(chan, this.currentPageIdx[chan]);
+  else
+    this.selectPage(chan, this.currentPageIdx[chan]+1);
+
 };
-ControlPages.prototype.selectPrev = function () {
-  this.selectPage(this.currentPageIdx-1);
+ControlPages.prototype.selectPrev = function (chan) {
+  if (!chan) chan = this.currentPage.channel;
+  if (chan != this.currentPage.channel)
+      this.selectPage(chan, this.currentPageIdx[chan]);
+  else
+    this.selectPage(chan, this.currentPageIdx[chan]-1);
 };
 ControlPages.prototype.indicateGUI = function (val) {
-  if (!this.currentPage) return;
-  // indicate all the controls from the 'mapControl' action
-  var mapped_ctrls = ['mapControl', 'mapControlRel']
-  for (var m in mapped_ctrls) {
-    m = this.currentPage.cc_map[mapped_ctrls[m]];
-    for (var cc in m) {
-      var cb = m[cc][0],
-          cc_idx = m[cc][1];
-      cb(cc_idx).setIndication(val);
+  if (!this.currentPage) { // no page, indicate usercontrols
+    for (var cc = LOWEST_CC; cc <= HIGHEST_CC; cc++) {
+      this.getUserControl(this.currentChannel, cc).setIndication(val);
     }
-  }
-  // And also all registered indication callbacks
-  for (var i in this.currentPage.indicate_cbs) {
-    this.currentPage.indicate_cbs[i](val);
+  } else {
+    // indicate all maped controls
+    for (var cc in this.currentPage.ccMap) {
+      var control_data = this.currentPage.ccMap[cc];
+      // 0: is the function that returns the bitwig control
+      // 1: is the index into the given cc array
+      control_data[0].setIndication(val);
+    }
+    // indicate all other controls supplied
+    for (var i in this.currentPage.indicateCtrls) {
+      this.currentPage.indicateCtrls[i].setIndication(val);
+    }
   }
 };
-ControlPages.prototype.onMidi = function (status, data1, data2) {
-  if (!this.currentPage || !isChannelController(status)) return;
 
-  var cc = data1, val = data2;
-  // printMidi(status, data1, data2);
+ControlPages.prototype.getUserControl = function (channel, cc) {
+  return this.userControls.getControl((cc - LOWEST_CC) + channel * 16);
+}
+ControlPages.prototype.setUserControlVal = function (channel, cc, val) {
+  this.getUserControl(channel, cc).set(val, MIDI_MAX_VAL);
+}
 
-  if (!this.enabled && !this.currentPage.always_ccs[cc]) {
-    return;
-  } else { // operate normally, if enabled
-    for (var action in this.currentPage.cc_map) {
-      action_cb = this.currentPage.cc_map[action][cc];
-      if (action_cb) {
-        var cc_idx = action_cb[1];
-        action_cb = action_cb[0];
-        this.action_map[action](action_cb, cc_idx, val, this.cc_val_history[cc]);
+ControlPages.prototype.performAction = function (page, chan, cc, val, hist) {
+  if (this.enabled || page.ccAlways[cc]) {
+    var ctrl = page.ccMap[cc];
+    // if it's a mapped ctrl, change it
+    if (ctrl) {
+      if (ctrl[1]) { // absolute control
+        ctrl[0].set(val, MIDI_MAX_VAL);
+      } else { // relative control
+        //FIXME is this responsie enough??
+        // this misses the first midi message (as there's no history)
+        if (hist)
+          ctrl[0].inc(val-hist, MIDI_MAX_VAL);
       }
+      return true;
     }
-    this.cc_val_history[cc] = val;
+    // if there's a callback, call it
+    ctrl = page.ccCallback[cc];
+    if (ctrl) {
+      ctrl[0](ctrl[1], chan, cc, val, hist);
+    }
   }
+  return false;
+}
+
+ControlPages.prototype.onMidi = function (status, data1, data2) {
+  var chan = status & 0xF, cc = data1, val = data2;
+  var hist = this.ccValHistory[cc];
+  var found_action = false;
+
+  this.currentChannel = chan;
+
+  if (this.currentPage && this.currentPage.channel == chan) {
+    found_action = this.performAction(this.currentPage, chan, cc, val, hist);
+  } else if (this.channelPages[chan]) {
+    this.selectPage(chan, this.currentPageIdx[chan]);
+    return this.onMidi(status, data1, data2);
+  } else if (this.currentPage) {
+    // no pages in this channel
+    this.selectPage(chan, 0);
+  }
+
+  if (!found_action) {
+    // this is a page-less channel or an un-mapped cc in this page
+    // check for and try to perform globally mapped action
+    if (!this.performAction(this.globalPage, chan, cc, val, hist))
+      // otherwise map to a user-control
+      this.setUserControlVal(chan, cc, val);
+  }
+
+  // save cc history
+  this.ccValHistory[cc] = val;
 };
